27b20df5110d405ffce355c1b53b8321a8c8532c
Pearlicia <ebikonpearl@gmail.com>
2019-11-21 18:11:32 +0100

    working on search input

---
+++ public/location.js
@@ -0,0 +1,24 @@
+let searchBox;
+
+
+const searchElement = document.querySelector('[city-search]')
+searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () => {
+    const place = searchBox.getPlaces()[0]
+    if (place == null) return
+    const latitude = place.geometry.location.lat()
+    const longitude = place.geometry.location.lng()
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            latitude: latitude,
+            longitude: longitude
+        })
+    }).then(res => res.json()).then(data => {
+        setWeatherData(data, place.formatted_address)
+    })
+})
---
+++ server.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => {
+    // const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+    console.log(req.body)
+})
+
+app.listen(3000, () => {
+    console.log('Server Started')
+})
