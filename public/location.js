ac5d7bd2491ee27651525eeb2e4e80f6ac38deb1
Pearlicia <ebikonpearl@gmail.com>
2019-11-27 12:56:53 +0100

    styled weather detail section

--- public/location.js
+++ public/location.js
@@ -1,24 +1,45 @@
-let searchBox;
 
-
-const searchElement = document.querySelector('[city-search]')
-searchBox = new google.maps.places.SearchBox(searchElement)
-searchBox.addListener('places_changed', () => {
-    const place = searchBox.getPlaces()[0]
-    if (place == null) return
-    const latitude = place.geometry.location.lat()
-    const longitude = place.geometry.location.lng()
-    fetch('/weather', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            latitude: latitude,
-            longitude: longitude
+    function initMap() {
+                
+        const searchElement = document.querySelector('[city-search]')
+        searchBox = new google.maps.places.SearchBox(searchElement)
+        searchBox.addListener('places_changed', () => {
+            const place = searchBox.getPlaces()[0]
+            if (place == null) return
+            const latitude = place.geometry.location.lat()
+            const longitude = place.geometry.location.lng()
+            fetch('/weather', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    latitude: latitude,
+                    longitude: longitude
+                })
+            }).then(res => {
+                console.log(res)
+                // console.log(res.json())
+            })
+            .then(data => {
+                setWeatherData(data, place.formatted_address)
+            })
         })
-    }).then(res => res.json()).then(data => {
-        setWeatherData(data, place.formatted_address)
-    })
-})
+
+        const locationElement = document.querySelector('[data-location]')
+        const statusElement = document.querySelector('[data-status]')
+        const temperatureElement = document.querySelector('[data-temperature]')
+        const precipitationElement = document.querySelector('[data-precipitation]')
+        const windElement = document.querySelector('[data-wind]')
+
+        function setWeatherData(data, place){
+            locationElement.textContent = place
+            statusElement.textContent = data.summary
+            temperatureElement.textContent = data.temperature
+            precipitationElement.textContent = `${data.precipProbability * 100}%`
+            windElement.textContent = data.windSpeed
+        }
+        
+
+    }
